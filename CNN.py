8cce7161dc477bfe0d19a50a7b1fcd69e9aa7d5a
Ryan Cho <cho.ryan212@gmail.com>
2018-07-12 15:35:44 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -6,9 +6,17 @@ Implementation of Keras and Tensorflow
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
-
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 def run_cnn():
+    learning_rate = 0.0001
+    epochs = 10
+    batch_size = 50
+
+    x = tf.placeholder(tf.float32, [None, 784])
+    x_shaped = tf.reshape(x, [-1, 28, 28, 1])
+    y = tf.placeholder(tf.float32, [None, 10])
+
     layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
     layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
     
@@ -61,6 +69,7 @@ def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_sh
     out_layer += bias
     out_layer = tf.nn.relu(out_layer)
     ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, 2, 2, 1]
     out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
 
     return out_layer
